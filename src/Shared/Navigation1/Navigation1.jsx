eac44c1c9d17e9c5ecee438a7370586f2484bb6c
tarekul42 <tarekulrifat205@gmail.com>
2023-10-11 02:19:28 -0700

    search bar icon added

--- src/Shared/Navigation1/Navigation1.jsx
+++ src/Shared/Navigation1/Navigation1.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ResponsiveNav from '../responsiveNav/ResponsiveNav';
 import CommonNav from '../CommonNav/CommonNav';
-import { FaBeer } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
 const Navigation1 = () => {
     return (
         <div className="navbar justify-around">
             <ResponsiveNav/>
             <div className="hidden lg:flex">
-                <div className="form-control px-2 text-white">
-                    <input type="text" id='search' placeholder={`<FaBeer /> Search your Destination...`} className="input input-ghost w-96 h-11 p-3 max-w-xs border-2 border-slate-100" />
+                <div className="form-control px-2 text-white relative">
+                    <input type="text" id='search' placeholder={`Search your Destination...`} className="input input-ghost w-96 h-11 px-8 py-3 max-w-xs border-2 border-slate-100" />
+                    <FaSearch className='absolute top-4 bottom-4 left-5' />
                 </div>
                 <CommonNav/>
             </div>
